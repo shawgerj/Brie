ae26e2b89736b615c3ff080170237d252aa6aca7
John Shawger <shawgerj@gmail.com>
2012-07-23 11:41:56 -0500

    Rewrote views.py using django view shortcuts. Much nicer now.

--- ricotta/urls.py
+++ ricotta/urls.py
@@ -18,6 +18,6 @@ urlpatterns = patterns('',
                        
     url(r'^calendar/$', 'ricotta.views.calendar_base', name='calendar-home'),
     url(r'^calendar/(?P<location_name>.*)/$', 'ricotta.views.calendar'),
-    url(r'^planner/(?P<username>\w+)/$', 'ricotta.views.planner'),
+    url(r'^planner/(?P<username>\w+)/$', 'ricotta.views.planner', name='edit-own-planner'),
     url(r'^planner/lab/(?P<location_name>\w+)/$', 'ricotta.views.planner_lab'),
 )
--- ricotta/views.py
+++ ricotta/views.py
@@ -2,75 +2,52 @@ from django.template import RequestContext, Context, loader
 from ricotta.models import Shift, Location, UserProfile, PlannerBlock
 from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 
 def home(request):
-    t = loader.get_template('ricotta/home.html')
-    c = RequestContext(request)
-    return HttpResponse(t.render(c))
+    return render(request, 'ricotta/home.html', 
+                  {"worker": request.user.username})
             
 def shifts_by_user(request, username):
     shift_list = Shift.objects.filter(worker__username__exact=username).order_by('start_time')
-    t = loader.get_template('ricotta/shifts.html')
-    c = Context({
-        'shift_list': shift_list,
-        'user': username,
-    })
-    return HttpResponse(t.render(c))
+
+    return render(request, 'ricotta/shifts.html', 
+                  {"shift_list": shift_list,
+                   "user": username})
 
 def locations(request):
     user_lab_count = UserProfile.objects.values('lab').annotate(Count('lab'))
-    
-    t = loader.get_template('ricotta/locations.html')
-    c = Context({
-            'user_lab_count': user_lab_count,
-    })
-    return HttpResponse(t.render(c))
+
+    return render(request, 'ricotta/locations.html',
+                  {"user_lab_count": user_lab_count})
 
 def calendar_base(request):
     calendars = Location.objects.all()
-    t = loader.get_template('ricotta/calendar_none.html')
-    c = RequestContext(request, {
-            'calendars': calendars,
-            })
-    return HttpResponse(t.render(c))
+    return render(request, 'ricotta/calendar_none.html', 
+                  {"calendars": calendars})
 
 def calendar(request, location_name):
-    try:
-        l = Location.objects.get(pk=location_name)
-    except:
-        raise Http404
-    else:
-        t = loader.get_template('ricotta/calendar.html')
-        c = RequestContext(request, {
-                'location_name': location_name,
-            })
-        return HttpResponse(t.render(c))
+    get_object_or_404(Location, pk=location_name)
+
+    return render(request, 'ricotta/calendar.html', 
+                  {"location_name": location_name})
 
 def planner(request, username):
-    try:
-        User.objects.get(username=username)
-    except:
-        raise Http404
-    else:
-        t = loader.get_template('ricotta/planner.html')
-        c = RequestContext(request, {
-                'worker': username,
-                'preferences': PlannerBlock.PLANNER_CHOICES,
-            })
-        return HttpResponse(t.render(c))
+    get_object_or_404(User, username=username)
+
+    return render(request, 'ricotta/planner.html',
+                  {"worker": username, 
+                   "preferences": PlannerBlock.PLANNER_CHOICES})
 
 def planner_lab(request, location_name):
-    try:
-        Location.objects.get(pk=location_name)
-    except:
-        raise Http404
-    else:
-        workers = UserProfile.objects.filter(lab=location_name)
-        
-        t = loader.get_template('ricotta/planner_lab.html')
-        c = RequestContext(request, {
-                'workers': workers,
-                'location_name': location_name
-            })
-        return HttpResponse(t.render(c))
+    get_object_or_404(Location, pk=location_name)
+    workers = UserProfile.objects.filter(lab=location_name)
+    
+    return render(request, 'ricotta/planner_lab.html', 
+                  {"workers": workers,
+                   "location_name": location_name})
+
+def timeclock(request):
+    return render(request, 'ricotta/timeclock.html',
+                  {"worker": request.user.username})
