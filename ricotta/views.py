734cb745e3a1a829a49452e713f38713f1b5446a
John Shawger <shawgerj@gmail.com>
2012-07-25 14:36:30 -0500

    Further improvements to the timeclock view. Total scheduled and clocked hours are now displayed.

--- ricotta/views.py
+++ ricotta/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
+from datetime import datetime, timedelta
 
 def home(request):
     return render(request, 'ricotta/home.html', 
@@ -38,11 +39,27 @@ def planner_lab(request, location_name):
                    "location_name": location_name})
 
 def timeclock(request, username):
+    # this all needs to be queried on 2 week intervals eventually rather
+    # than pulling the entire result set
     tr_data = TimeclockRecord.objects.filter(employee=request.user)
+    sh_data = Shift.objects.filter(worker=request.user)
+
+    # I bet there's a more clever way of doing this. One-liner perhaps?
+    total_clocked = timedelta(0)
+    for tr in tr_data:
+        total_clocked += (tr.end_time - tr.start_time)
+        
+    # and here too
+    total_scheduled = timedelta(0)
+    for sh in sh_data:
+        total_scheduled += (sh.end_time - sh.start_time)
+        
 
     return render(request, 'ricotta/timeclock.html',
                   {"worker": request.user.username,
-                   "tr_data": tr_data})
+                   "tr_data": tr_data,
+                   "total_scheduled": round(total_scheduled.total_seconds() / 3600, 2),
+                   "total_clocked": round(total_clocked.total_seconds() / 3600, 2)})
 
 def employees(request):
     employees = User.objects.all()
