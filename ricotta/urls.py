3cb4e670d44db88fdca3a7f57a1db0fb1dbd1734
John Shawger <shawgerj@gmail.com>
2012-07-31 12:00:34 -0500

    Fixed #5 and #2

--- ricotta/urls.py
+++ ricotta/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'ricotta.views.home'),
     url(r'^clockin/$', 'ricotta.views.clockin', name='clock-in'),
-    
+
     url(r'^calendar/$', 'ricotta.views.calendar_base', name='calendar-home'),
     url(r'^calendar/(?P<location_name>.*)/$', 'ricotta.views.calendar'),
     url(r'^shifts/(?P<username>\w+)/$', 'ricotta.views.shifts', name='my-shifts'),
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^planner/lab/(?P<location_name>\w+)/$', 'ricotta.views.planner_lab'),
 
     url(r'^timeclock/(?P<username>\w+)/$', 'ricotta.views.timeclock', name='view-timeclock'),
+    url(r'^timeclock/(?P<username>\w+)/(?P<pastperiod>\d+)/$', 'ricotta.views.timeclock', name='view-timeclock'),
     url(r'^whos_clockin/$', 'ricotta.views.whos_clockin', name='whos-clockin'),
 
     url(r'^employees/$', 'ricotta.views.employees', name='employees'),
--- ricotta/views.py
+++ ricotta/views.py
@@ -8,13 +8,13 @@ from datetime import timedelta
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'ricotta/home.html', 
+    return render(request, 'ricotta/home.html',
                   {"worker": request.user.username,
                    "lab": request.user.profile.lab})
 
 def clockin(request):
     ta = TimeclockAction.objects.filter(employee=request.user)
-    
+
     try:
         real_ip = request.META['HTTP_X_FORWARDED_FOR']
     except KeyError:
@@ -38,18 +38,18 @@ def clockin(request):
 def whos_clockin(request):
     clocked_in = TimeclockAction.objects.all()
     return render(request, 'ricotta/whos_clockin.html',
-                  {"clocked_in": clocked_in, 
+                  {"clocked_in": clocked_in,
                    "title": "Who's Clocked In"})
-            
+
 def calendar_base(request):
     calendars = Location.objects.all()
-    return render(request, 'ricotta/calendar_none.html', 
+    return render(request, 'ricotta/calendar_none.html',
                   {"calendars": calendars})
 
 def calendar(request, location_name):
     get_object_or_404(Location, pk=location_name)
 
-    return render(request, 'ricotta/calendar.html', 
+    return render(request, 'ricotta/calendar.html',
                   {"location_name": location_name,
                    "worker": request.user})
 
@@ -57,42 +57,47 @@ def planner(request, username):
     get_object_or_404(User, username=username)
 
     return render(request, 'ricotta/planner.html',
-                  {"worker": request.user, 
+                  {"worker": request.user,
                    "preferences": PlannerBlock.PLANNER_CHOICES})
 
 def planner_lab(request, location_name):
     get_object_or_404(Location, pk=location_name)
     workers = UserProfile.objects.filter(lab=location_name)
-    
-    return render(request, 'ricotta/planner_lab.html', 
+
+    return render(request, 'ricotta/planner_lab.html',
                   {"workers": workers,
                    "location_name": location_name})
 
-def timeclock(request, username):
+def timeclock(request, username, pastperiod=0):
+    pastperiod = int(pastperiod)
     # this all needs to be queried on 2 week intervals eventually rather
     # than pulling the entire result set
-    tr_data = TimeclockRecord.objects.filter(employee=request.user)
-    sh_data = Shift.objects.filter(worker=request.user)
+    start_bound = timezone.now() - timedelta(weeks=pastperiod * 2) - timedelta(weeks=2)
+    end_bound = timezone.now() - timedelta(weeks=pastperiod * 2)
+    tr_data = TimeclockRecord.objects.filter(employee=request.user).filter(start_time__range=(start_bound, end_bound))
+    sh_data = Shift.objects.filter(worker=request.user).filter(start_time__range=(start_bound, end_bound))
 
     # I bet there's a more clever way of doing this. One-liner perhaps?
     total_clocked = timedelta(0)
     for tr in tr_data:
         total_clocked += (tr.end_time - tr.start_time)
-        
+
     # and here too
     total_scheduled = timedelta(0)
     for sh in sh_data:
         total_scheduled += (sh.end_time - sh.start_time)
-        
+
 
     return render(request, 'ricotta/timeclock.html',
                   {"worker": request.user.username,
                    "tr_data": tr_data,
                    "total_scheduled": round(total_scheduled.total_seconds() / 3600, 2),
-                   "total_clocked": round(total_clocked.total_seconds() / 3600, 2)})
+                   "total_clocked": round(total_clocked.total_seconds() / 3600, 2),
+                   "title": "Timeclock for " + request.user.username,
+                   "pastperiod": pastperiod})
 
 def shifts(request, username):
-    shift_data = Shift.objects.filter(worker=request.user)
+    shift_data = Shift.objects.filter(worker=request.user).filter(start_time__range=(timezone.now(), timezone.now() + timedelta(weeks=1)))
 
     return render(request, 'ricotta/shift.html',
                   {"worker": request.user.username,
@@ -101,13 +106,13 @@ def shifts(request, username):
 def employees(request):
     employees = User.objects.all()
 
-    return render(request, 'ricotta/employees.html', 
-                  {"employees": employees, 
+    return render(request, 'ricotta/employees.html',
+                  {"employees": employees,
                    "title": "All Employees"})
 
 def employees_by_lab(request, location_name):
     employees = User.objects.filter(userprofile__lab=location_name)
-    
-    return render(request, 'ricotta/employees.html', 
+
+    return render(request, 'ricotta/employees.html',
                   {"employees": employees,
                    "title": "Employees in " + location_name})
